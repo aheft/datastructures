caac08c9a97aa726fe0616c40e2545a63fec9650
AJ Heft <aheft9@yahoo.ca>
2020-11-25 17:40:56 -0500

    A7

--- Assignment 7/testmain.c
+++ Assignment 7/testmain.c
@@ -8,13 +8,13 @@
 int main(){
     init_boards();
     join_graph(START_BOARD);
-    print_node(htable[board_hash(START_BOARD)]);
+    /*print_node(htable[board_hash(START_BOARD)]);
     START_BOARD[pos2idx[0]]='X';
     print_node(htable[board_hash(START_BOARD)]);
     START_BOARD[pos2idx[8]]='O';
     START_BOARD[pos2idx[1]]='X';
     START_BOARD[pos2idx[7]]='O';
     START_BOARD[pos2idx[2]]='X';
-    print_node(htable[board_hash(START_BOARD)]);
+    print_node(htable[board_hash(START_BOARD)]);*/
     return 0;
 }
